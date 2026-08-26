a45f17d15c7941b7de9416f81b893e2beb8c8ee3
Camille1358 <poupartprin@gmail.com>
2026-08-26 15:24:21 +0200

    Add Vencord support for Discord and PTB packages

--- Packages/packages_2.nix
+++ Packages/packages_2.nix
@@ -2,6 +2,13 @@
 #logiciel
 {
   environment.systemPackages = with pkgs; [
+  (discord.override {
+    withVencord = true;
+  })
 
+  # Discord PTB + Vencord
+  (discord-ptb.override {
+    withVencord = true;
+  })
   ];
 }
